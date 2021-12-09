f68448128b16f53ef73032d50ea0d555519b6901
antoniom28 <antomarc@gmail.com>
2021-12-09 13:48:26 +0100

    aggiunto style per i "button" con js

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
 let element = document.querySelector('.header-right .fas.fa-bars');
-element.addEventListener('click', function() {
-    document.querySelector('.hamburger-menu').style.display = 'block'
-  });
+element.style.transform = 'scale(1.5)';
+element.style.marginRight = '5px';
 
-document.querySelector('.close').addEventListener('click',function(){
+element.addEventListener('click', function () {
+    document.querySelector('.hamburger-menu').style.display = 'block'
+});
+
+element.addEventListener('mousemove', function () {
+    element.style.transform = 'scale(1.7)';
+});
+
+element.addEventListener('mouseleave', function () {
+    element.style.transform = 'scale(1.5)';
+});
+
+document.querySelector('.close').addEventListener('click', function () {
     document.querySelector('.hamburger-menu').style.display = 'none'
+});
+
+document.querySelector('.close').addEventListener('mousemove', function () {
+    document.querySelector('.close').style.transform = 'scale(1.2)';
+});
+
+document.querySelector('.close').addEventListener('mouseleave', function () {
+    document.querySelector('.close').style.transform = 'scale(1)';
 });
